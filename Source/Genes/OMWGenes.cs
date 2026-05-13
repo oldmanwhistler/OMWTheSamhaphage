e6582cdc473eab4881ebe4c2f69037db5d56686e
Old Man Whistler <262932034+oldmanwhistler@users.noreply.github.com>
2026-05-12 23:46:05 -0400

    Fixed resurrecting as echovessel so it doesn't wipe out stuff from previous random genes

--- Source/Genes/OMWGenes.cs
+++ Source/Genes/OMWGenes.cs
@@ -102,16 +102,11 @@ namespace OMW_Samhaphage
             }
         }        
 
-        public static void XenogenesToEndogenes(Pawn pawn, bool removeDisabledFirst=true)        
+        public static void XenogenesToEndogenes(Pawn pawn)        
         {
             if (pawn == null || pawn.genes == null) return;
 
             if (pawn.genes.Xenogenes.Count == 0) return;
-
-            if (removeDisabledFirst)
-            {
-                RemoveDisabledGenes(pawn);
-            }
 
             // 1. Snapshot the Xenogenes (copy to avoid modification-during-enumeration errors)
             List<GeneDef> genesToMove = new List<GeneDef>();
@@ -129,81 +124,23 @@ namespace OMW_Samhaphage
         }
 
 
-        public static bool RemoveXenogenes(Pawn source)
-        {
-            if (source == null || source.genes == null)
-                return false;
-
-            List<Gene> genesToRemove = new List<Gene>();
-            foreach (Gene xenoGene in source.genes.Xenogenes)
-            {
-                genesToRemove.Add(xenoGene);
-            }
-
-            // We start at the last index and move toward 0
-            foreach (Gene gene in genesToRemove)
-            {
-                source.genes.RemoveGene(gene);
-            }            
-
-            OMWGenes.Refresh(source);
-
-            if (debug)
-                Log.Message(
-                    $"{source.LabelShort}.RemoveXenogenes: Removed {genesToRemove.Count} xenogenes");
-            return true;
-        }
-
-        public static bool CopyXenogenes(Pawn source, Pawn destination)
-        {
-            if (source == null || destination == null || source.genes == null || destination.genes == null) return false;
-
-            List<GeneDef> genesToCopy = new List<GeneDef>();
-            foreach (Gene xenoGene in source.genes.Xenogenes)
-            {
-                genesToCopy.Add(xenoGene.def);
-            }
-
-            foreach (GeneDef geneDef in genesToCopy)
-            {
-                destination.genes.AddGene(geneDef, true);
-            }
-
-            source.genes.ClearXenogenes();
-            OMWGenes.Refresh(source);
-            OMWGenes.Refresh(destination);
-
-            if (debug) Log.Message($"{source.LabelShort}.CopyXenogenes to {destination.LabelShort}: Copied {genesToCopy.Count} xenogenes");
-            return true;
-        }
-
-        public static bool StealXenogenes(Pawn source, Pawn destination)
-        {
-            if (debug)
-                Log.Message(
-                    $"{destination.LabelShort}.TakeXenogenes from {destination.LabelShort}: Start");
-
-            if (!CopyXenogenes(source, destination)) return false;
-            if (!RemoveXenogenes(source)) return false;
-
-            if (debug)
-                Log.Message(
-                    $"{destination.LabelShort}.TakeXenogenes from {destination.LabelShort}: Done");
-            return true;
-        }        
-
         public static void AddXenotype(Pawn pawn, XenotypeDef xenotype)
         {
             if (pawn == null || xenotype == null) return;
 
-            // Clear existing xenogenes first if you want a clean override
-            if (pawn?.genes != null)
-            {
-                pawn.genes.ClearXenogenes();
-            }
-
             // Update the label so the UI shows the correct Xenotype name
             pawn.genes.SetXenotypeDirect(xenotype);
+
+            List<Gene> genesToRemove = new List<Gene>();
+            foreach (Gene xenoGene in pawn.genes.Xenogenes)
+            {
+                genesToRemove.Add(xenoGene);
+            }
+
+            foreach (Gene gene in genesToRemove)
+            {
+                pawn.genes.RemoveGene(gene);
+            }     
 
             int count = 0;            
             // Add all genes from the new xenotype as Xenogenes
@@ -212,9 +149,13 @@ namespace OMW_Samhaphage
                 pawn.genes.AddGene(geneDef, xenogene: true);
                 count++;
             }
+            foreach (Gene gene in genesToRemove)
+            {
+                pawn.genes.AddGene(gene.def, xenogene: true);
+            }     
 
             Refresh(pawn);
-            if (debug) Log.Message($"{pawn.LabelShort}.AddXenotype: Added {count} genes from xenotype {xenotype.LabelCap}");
+            if (debug) Log.Message($"{pawn.LabelShort}.AddXenotype: Added {count} genes from xenotype {xenotype.LabelCap} to the front of xenogenes");
         }
 
         public static void RemoveXenotype(Pawn pawn, XenotypeDef xenotype)
@@ -229,9 +170,10 @@ namespace OMW_Samhaphage
                 {
                     pawn.genes.RemoveGene(gene);
                     count++;
-                }                
+                }
             }
 
+            // this sets the xenotype name
             pawn.genes.SetXenotypeDirect(null);
             Refresh(pawn);
             if (debug) Log.Message($"{pawn.LabelShort}.RemoveXenotype: Removed {count} genes and xenotype {xenotype.LabelCap}");
@@ -258,15 +200,14 @@ namespace OMW_Samhaphage
 
         public static bool ChangeXenotype(Pawn pawn, XenotypeDef sourceXenotype, XenotypeDef targetXenotype)
         {
+            if (sourceXenotype == null) sourceXenotype = pawn.genes?.Xenotype;
             if (!CanChangeXenotype(pawn, sourceXenotype, targetXenotype, true)) return false;
             // Guess the xenotype
-            if (sourceXenotype == null) sourceXenotype = pawn.genes?.Xenotype;
+
             if (debug) Log.Message($"{pawn.LabelShort}.ChangeXenotype: Start changing from {sourceXenotype?.LabelCap ?? "null"} to {targetXenotype?.LabelCap ?? "null"}");
             if (sourceXenotype != null) RemoveXenotype(pawn, sourceXenotype);
-            RemoveDisabledGenes(pawn);
             XenogenesToEndogenes(pawn);
             if (targetXenotype != null) AddXenotype(pawn, targetXenotype);
-            RemoveDisabledGenes(pawn);
             if (debug)
                 Log.Message(
                     $"{pawn.LabelShort}.ChangeXenotype: Done changing from {sourceXenotype?.LabelCap ?? "null"} to {targetXenotype?.LabelCap ?? "null"}");
@@ -281,7 +222,6 @@ namespace OMW_Samhaphage
             if (debug) Log.Message($"{pawn.LabelShort}.ChangeEndotype: Start changing from {sourceXenotype?.LabelCap ?? "null"} to {targetXenotype?.LabelCap ?? "null"}");
             ChangeXenotype(pawn, sourceXenotype, targetXenotype);
             XenogenesToEndogenes(pawn);
-            RemoveDisabledGenes(pawn);
             if (debug)
                 Log.Message(
                     $"{pawn.LabelShort}.ChangeEndotype: Done changing from {sourceXenotype?.LabelCap ?? "null"} to {targetXenotype?.LabelCap ?? "null"}");
