d3d2ae72e816bb27b3668f9fba5c21c2adb07517
Old Man Whistler <262932034+oldmanwhistler@users.noreply.github.com>
2026-06-06 00:28:13 -0400

    change it to a row

--- Source/Window/BetterFloatMenu.cs
+++ Source/Window/BetterFloatMenu.cs
@@ -114,12 +114,10 @@ namespace OMW_Samhaphage
         /// </summary>
         public bool CanSearch = true;
         /// <summary>
-        /// How many columns of items to display.
-        /// Items are split equally between columns.
-        /// The width of a column is determined by the widest item in the column.
-        /// Default value: 2.
+        /// How many items to display per row.
+        /// Default value: 4.
         /// </summary>
-        public int Columns = 2;
+        public int Columns = 4;
         /// <summary>
         /// The amount of padding between items, measured in unscaled pixels.
         /// Default value: 6.
@@ -167,59 +165,59 @@ namespace OMW_Samhaphage
                 preRenderItems.AddRange(FilteredItems(SearchString));
             }
 
-            int perColumnTarget = Mathf.CeilToInt((float)preRenderItems.Count / Columns);
-
-            float x = 0;
+            float curX = 0;
+            float curY = 0;
+            float maxRowHeight = 0;
+            int columnCount = 0;
 
             Widgets.BeginScrollView(inRect, ref scroll, new Rect(0, 0, lastWidth, lastHeight));
             lastWidth = 0;
             lastHeight = 0;
 
-            for (int i = 0; i < Columns; i++)
+            for (int i = 0; i < preRenderItems.Count; i++)
             {
-                float maxItemWidth = 0f;
-                float y = 0;
-                for (int j = 0; j < perColumnTarget; j++)
+                var item = preRenderItems[i];
+                var pos = new Vector2(curX, curY);
+                var size = item.Draw(pos);
+                var area = new Rect(pos, size);
+
+                if (item.BoxThickness > 0 && item.BoxColor.a > 0)
                 {
-                    int index = perColumnTarget * i + j;
-                    if (index >= preRenderItems.Count)
-                        break;
+                    GUI.color = item.BoxColor;
+                    Widgets.DrawBox(area, item.BoxThickness);
+                    GUI.color = Color.white;
+                }
 
-                    var pos = new Vector2(x, y);
-                    var item = preRenderItems[index];
-                    var size = item.Draw(pos);
-                    var area = new Rect(pos, size);
-
-                    if (item.BoxThickness > 0 && item.BoxColor.a > 0)
+                if (!item.Disabled)
+                {
+                    Widgets.DrawHighlightIfMouseover(area);
+                    if (Widgets.ButtonInvisible(area))
                     {
-                        GUI.color = item.BoxColor;
-                        Widgets.DrawBox(area, item.BoxThickness);
-                        GUI.color = Color.white;
-                    }
-
-                    if (!item.Disabled)
-                    {
-                        Widgets.DrawHighlightIfMouseover(area);
-                        if (Widgets.ButtonInvisible(area))
+                        SoundDefOf.Click.PlayOneShotOnCamera();
+                        OnSelected?.Invoke(item);
+                        if (CloseOnSelected)
                         {
-                            SoundDefOf.Click.PlayOneShotOnCamera();
-                            OnSelected?.Invoke(item);
-                            if (CloseOnSelected)
-                            {
-                                Close();
-                                break;
-                            }
+                            Close();
+                            break;
                         }
                     }
-                    y += size.y + Padding;
-                    if (maxItemWidth < size.x)
-                        maxItemWidth = size.x;
-                    if (y > lastHeight)
-                        lastHeight = y;
                 }
-                x += maxItemWidth + Padding * 2;
-                if (x > lastWidth)
-                    lastWidth = x;
+
+                curX += size.x + Padding * 2;
+                maxRowHeight = Mathf.Max(maxRowHeight, size.y);
+                columnCount++;
+
+                if (columnCount >= Columns)
+                {
+                    lastWidth = Mathf.Max(lastWidth, curX);
+                    curX = 0;
+                    curY += maxRowHeight + Padding;
+                    maxRowHeight = 0;
+                    columnCount = 0;
+                }
+
+                lastHeight = Mathf.Max(lastHeight, curY + maxRowHeight);
+                lastWidth = Mathf.Max(lastWidth, curX);
             }
 
             Widgets.EndScrollView();
@@ -295,101 +293,6 @@ namespace OMW_Samhaphage
         /// <param name="pos">The input position. This is the top-left, and it is in GUI space.</param>
         /// <returns>The size of this drawn item. Should not be negative.</returns>
         public abstract Vector2 Draw(Vector2 pos);
-    }
-
-    /// <summary>
-    /// A <see cref="MenuItemText"/> that displays a label and optionally an icon next to it.
-    /// Can also have a tooltip. Has a fixed size.
-    /// </summary>
-    public class MenuItemText : MenuItemBase
-    {
-        /// <summary>
-        /// The label to display.
-        /// </summary>
-        public string Label;
-        /// <summary>
-        /// The tooltip to display. May be null.
-        /// </summary>
-        public string Tooltip;
-        /// <summary>
-        /// The icon to display. May be null.
-        /// </summary>
-        public Texture2D Icon;
-        /// <summary>
-        /// The tint of the icon.
-        /// </summary>
-        public Color IconColor = Color.white;
-        /// <summary>
-        /// The size of the box, in pixels.
-        /// </summary>
-        public Vector2 Size = new Vector2(212, 28);
-
-        protected string drawLabel;
-        private bool consumedSearch = false;
-
-        public MenuItemText() { }
-
-        public MenuItemText(object payload, string text, Texture2D icon = null, Color iconColor = default, string tooltip = null)
-        {
-            this.Payload = payload;
-            this.Label = text;
-            this.Icon = icon;
-            this.Tooltip = tooltip;
-            this.IconColor = iconColor == default ? Color.white : iconColor;
-        }
-
-        public override bool MatchesSearch(string search)
-        {
-            drawLabel = BetterFloatMenu.SearchMatch(Label, search);
-            consumedSearch = false;
-            return drawLabel != null;
-        }
-
-        public override int CompareTo(MenuItemBase other)
-        {
-            if (other is MenuItemText txt)
-                return string.Compare(Label, txt.Label, StringComparison.Ordinal);
-            return 0;
-        }
-
-        public override Vector2 Draw(Vector2 pos)
-        {
-            Rect area = new Rect(pos, Size);
-
-            string label = Label;
-            if (!consumedSearch)
-            {
-                label = drawLabel;
-                consumedSearch = true;
-            }
-
-            bool hasIcon = Icon != null;
-
-            if (hasIcon)
-            {
-                Rect iconArea = area;
-                iconArea.width = iconArea.height;
-                GUI.color = Disabled ? Color.gray : IconColor;
-                Widgets.DrawTextureFitted(iconArea, Icon, 1f);
-                GUI.color = Color.white;
-            }
-
-            Rect labelArea = area;
-            labelArea.y += hasIcon ? 3 : 5;
-            if (hasIcon)
-                labelArea.xMin += area.height + 2;
-            else
-                labelArea.xMin += 4;
-
-            if (Disabled) GUI.color = Color.gray;
-            Widgets.LabelFit(labelArea, label);
-            GUI.color = Color.white;
-
-            if (Tooltip != null)
-                TooltipHandler.TipRegion(area, Tooltip);
-
-            return Size;
-        }
     }
 
     /// <summary>
